e6f71ddf5de6744d4b794d7f9f264f31d4e08b09
chuckrea <chuckr523@mac.com>
2014-04-03 15:40:47 -0400

    clean up

---
+++ javascripts/tickets.js
@@ -0,0 +1,21 @@
+function TicketCtrl($scope) {
+  $scope.tickets = [
+    {title:'Aliens', director:'James Cameron', seat:'156', time:'1:30'},
+    {title:'Star Wars', director:'George Lucas', seat:'G101', time:'Midnight'},
+    {title:'The Silence of the Lambs', director:'Jonathan Demme', seat:'19F', time:'2:30'}
+    ];
+
+  $scope.addTicket = function() {
+    $scope.tickets.push({title:$scope.ticketTitle, director:$scope.ticketDir, seat:$scope.ticketSeat, time:$scope.ticketTime});
+    $scope.ticketTitle = "";
+    $scope.ticketDir = "";
+    $scope.ticketSeat = "";
+    $scope.ticketTime = "";
+  };
+
+  $scope.removeTicket = function(idx) {
+    $scope.tickets.splice(idx, 1)
+  }
+
+
+}
